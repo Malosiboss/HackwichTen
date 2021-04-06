fda1c712fa6cd40e6377e410a0f0c24b6555e81f
NoahNua <NoahNua@hawaii.edu>
2021-04-06 10:15:20 -1000

    set up index 0-2

--- HackwichTen/ViewController.swift
+++ HackwichTen/ViewController.swift
@@ -9,10 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+       
         // Do any additional setup after loading the view.
+       
+        myLabel.text = "Index One Selected on the Segmented Control"
     }
+    @IBAction func segmentedControlPressed(_ sender: Any){
+        
+        switch segmentedControl.selectedSegmentIndex{
+        
+        
+        case 0:
+            myLabel.text = "Index One Selected on the Segmented Control"
+
+            
+        case 1:
+            myLabel.text = "Index Two selected. I got this!"
+            
+        case 2:
+            myLabel.text = "Woohoo, this makes sense now"
+            
+            
+            default:
+                break
+            }
+
+        
+    }
+    
 
 
 }
